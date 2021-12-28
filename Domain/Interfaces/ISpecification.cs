55198c01226422615489018509f83d44cd90e7d7
nicolas <nicolas.kelepuris@gmail.com>
2021-12-28 17:14:45 -0300

    add implementation of specificatio evaluator

--- Domain/Interfaces/ISpecification.cs
+++ Domain/Interfaces/ISpecification.cs
@@ -5,9 +5,9 @@ using System.Linq.Expressions;
 namespace Domain.Interfaces;
 public interface ISpecification<T>
 {
-    Expression<Func<T, bool>> Criteria { get; }
-    Expression<Func<T, object>> OrderBy { get; }
-    Expression<Func<T, object>> OrderByDescending { get; }
+    Expression<Func<T, bool>>? Criteria { get; }
+    Expression<Func<T, object>>? OrderBy { get; }
+    Expression<Func<T, object>>? OrderByDescending { get; }
     List<Expression<Func<T, object>>> Includes { get; }
     List<string> IncludesByString { get; }
     int Take { get; }
--- Persistence/Data/SpecificationEvaluator.cs
+++ Persistence/Data/SpecificationEvaluator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using Domain.Entities.Base;
 using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Persistence.Data;
 public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
@@ -15,5 +16,68 @@ public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
 
         this.query = query;
         _specification = specification;
-    }    
+    }
+
+    public IQueryable<TEntity> EvaluateForCount()
+    {
+        ApplyCriteria();
+
+        return query;
+    }
+
+    public IQueryable<TEntity> Evaluate()
+    {
+        ApplyCriteria();
+        ApplyOrderBy();
+        ApplyOrderByDescending();
+        ApplyIncludes();
+        ApplyPagination();
+
+        return query;
+    }
+
+    private void ApplyCriteria()
+    {
+        if (_specification.Criteria != null)
+        {
+            query = query.Where(_specification.Criteria);
+        }
+    }
+
+    private void ApplyOrderBy()
+    {
+        if (_specification.OrderBy != null)
+        {
+            query = query.OrderBy(_specification.OrderBy);
+        }
+    }
+
+    private void ApplyOrderByDescending()
+    {
+        if (_specification.OrderByDescending != null)
+        {
+            query = query.OrderByDescending(_specification.OrderByDescending);
+        }
+    }
+
+    private void ApplyPagination()
+    {
+        if (_specification.IsPaginationEnabled)
+        {
+            query = query.Skip(_specification.Skip).Take(_specification.Take);
+        }
+    }
+
+    private void ApplyIncludes()
+    {
+        if (_specification.Includes.Any())
+        {
+            query = _specification.Includes.Aggregate(query, (current, include) => current.Include(include));
+        }
+
+        if (_specification.IncludesByString.Any())
+        {
+            query = _specification.IncludesByString.Aggregate(query, (current, include) => current.Include(include));
+        }
+    }
 }
